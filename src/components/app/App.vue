15f9e80710bc1997881eb9f6fbcd993c3e566e59
Shokhruh <shokhruh1197@gmail.com>
2024-04-30 17:13:39 +0500

    Miunted

--- src/components/app/App.vue
+++ src/components/app/App.vue
@@ -24,88 +24,94 @@
 </template>
 
 <script>
-import AppInfo from "@/components/app-info/AppInfo.vue";
-import SearchPanel from "@/components/search-filter/SearchPanel.vue";
-import MovieFilter from "@/components/movie-filter/MovieFilter.vue";
-import MovieList from "@/components/movie-list/MovieList.vue";
-import AddMovie from "@/components/add-movie/AddMovie.vue";
-export default {
-  components: {
-    AppInfo,
-    SearchPanel,
-    MovieList,
-    AddMovie,
-    MovieFilter,
-  },
-  data() {
-    return {
-      movies: [
-        {
-          id: 1,
-          name: "Omar",
-          seen_count: 591,
-          liked: true,
-          favourited: true,
-        },
-        {
-          id: 2,
-          name: "Ertugrul",
-          seen_count: 998,
-          liked: false,
-          favourited: false,
-        },
-        {
-          id: 3,
-          name: "Empire of Osman",
-          seen_count: 710,
-          liked: true,
-          favourited: false,
-        },
-      ],
-      term: '',
-      filter: 'all',
-    };
-  },
-  methods: {
-    createMovie(item) {
-      this.movies.push(item);
+  import AppInfo from "@/components/app-info/AppInfo.vue";
+  import SearchPanel from "@/components/search-filter/SearchPanel.vue";
+  import MovieFilter from "@/components/movie-filter/MovieFilter.vue";
+  import MovieList from "@/components/movie-list/MovieList.vue";
+  import AddMovie from "@/components/add-movie/AddMovie.vue";
+  export default {
+    components: {
+      AppInfo,
+      SearchPanel,
+      MovieList,
+      AddMovie,
+      MovieFilter,
     },
-    onToggleHandler({ id, prop }) {
-      this.movies = this.movies.map((movie) => {
-        if (movie.id == id) {
-          return { ...movie, [prop]: !movie[prop] };
-        }
-        return movie;
-      });
+    data() {
+      return {
+        movies: [
+          {
+            id: 1,
+            name: "Omar",
+            seen_count: 591,
+            liked: true,
+            favourited: true,
+          },
+          {
+            id: 2,
+            name: "Ertugrul",
+            seen_count: 998,
+            liked: false,
+            favourited: false,
+          },
+          {
+            id: 3,
+            name: "Empire of Osman",
+            seen_count: 710,
+            liked: true,
+            favourited: false,
+          },
+        ],
+        term: '',
+        filter: 'all',
+      };
     },
-    onDeleteHandler(id) {
-      this.movies = this.movies.filter(c => c.id !== id)
-    },
-    onSearchHandler(arr, term) {
-      if(term.length == 0) {
-        return arr;
-      }
-
-      return arr.filter(c => c.name.toLowerCase().indexOf(term) > -1)
-    },
-    onFilterHandler(arr, filter) {
-      switch(filter) {
-        case 'popular':
-          return arr.filter(c => c.favourited);
-        case 'most_viewed':
-          return arr.filter(c => c.seen_count > 700)
-        default:
+    methods: {
+      createMovie(item) {
+        this.movies.push(item);
+      },
+      onToggleHandler({ id, prop }) {
+        this.movies = this.movies.map((movie) => {
+          if (movie.id == id) {
+            return { ...movie, [prop]: !movie[prop] };
+          }
+          return movie;
+        });
+      },
+      onDeleteHandler(id) {
+        this.movies = this.movies.filter(c => c.id !== id)
+      },
+      onSearchHandler(arr, term) {
+        if(term.length == 0) {
           return arr;
-      }
+        }
+
+        return arr.filter(c => c.name.toLowerCase().indexOf(term) > -1)
+      },
+      onFilterHandler(arr, filter) {
+        switch(filter) {
+          case 'popular':
+            return arr.filter(c => c.favourited);
+          case 'most_viewed':
+            return arr.filter(c => c.seen_count > 700)
+          default:
+            return arr;
+        }
+      },
+      updateFilterHandler(filter) {
+        this.filter = filter
+      },
+      updateTermHandler(term) {
+        this.term = term
+      },
+      logger() {
+        console.log("Mounted");
+      },
     },
-    updateFilterHandler(filter) {
-      this.filter = filter
+    mounted() {
+      this.logger()
     },
-    updateTermHandler(term) {
-      this.term = term
-    }
-  },
-};
+  }
 </script>
 
 <style>
